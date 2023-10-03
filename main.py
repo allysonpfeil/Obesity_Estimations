c6bfca3be6ed32eb520c46cb8e08b23ebeaf1ba0
Ally N Pfeil <114772538+allysonpfeil@users.noreply.github.com>
2023-10-03 13:33:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,11 @@ y = data['NObeyesdad']  #target variable 'NObeyesdad'
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 #create a Random Forest classifier
-rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+rf_classifier = RandomForestClassifier(n_estimators=400,
+                                       max_depth=20,
+                                       min_samples_leaf=1,
+                                       min_samples_split=2,
+                                       random_state=42)
 
 #train the classifier on the training data
 rf_classifier.fit(X_train, y_train)
